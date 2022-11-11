2e86e1674239a9cde92a4c29351c2ec0a9c3710b
Matheus Lopes <matheusaraujo.lopes02092016@gmail.com>
2022-11-11 17:08:30 -0300

    build: :memo: Mandando progresos dos requisitos 15,16 e 18

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,9 @@
 export const userLogin = 'userLogin';
 export const PLAYER_USER = 'PLAYER_USER';
+export const RESET_SCORE = 'RESET-SCORE';
 
 export const logIn = (value) => ({ type: userLogin, value });
 
 export const playerUser = (value) => ({ type: PLAYER_USER, value });
+
+export const resetScore = (value) => ({ type: RESET_SCORE, value });
