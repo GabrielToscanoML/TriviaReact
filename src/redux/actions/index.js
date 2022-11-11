de21ab0fab6771ac443ddae8a1c5cc1904565c7e
joao <joaopaz79@outlook.com>
2022-11-11 18:46:50 -0300

    [Feat] - :rocket: Finaliza requisito 14

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,9 +1,12 @@
 export const userLogin = 'userLogin';
 export const PLAYER_USER = 'PLAYER_USER';
 export const RESET_SCORE = 'RESET-SCORE';
+export const SUM_ASSERTIONS = 'SUM_ASSERTIONS';
 
 export const logIn = (value) => ({ type: userLogin, value });
 
 export const playerUser = (value) => ({ type: PLAYER_USER, value });
 
 export const resetScore = (value) => ({ type: RESET_SCORE, value });
+
+export const sumAssertion = (assertion) => ({ type: SUM_ASSERTIONS, payload: assertion });
