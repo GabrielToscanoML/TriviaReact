496d6a1c1004cf2e4190fdaf2f7050d76e1b3bb8
Gabriel Toscano <gabrieltoscano1@outlook.com>
2023-11-29 09:44:31 -0300

    Estilização da pagina de feedback feita

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 
+import '../styles/Feedback.css';
+
 class Feedback extends React.Component {
   returnToRankingPage = () => {
     const { history } = this.props;
@@ -15,40 +17,43 @@ class Feedback extends React.Component {
     return (
       <div>
         <Header />
+        <main className="feedback-main-container">
+          {
+            assertions < minAssertions
+              ? <h2 data-testid="feedback-text" className="red">Could be better... </h2>
+              : <h2 data-testid="feedback-text" className="green">Well Done! </h2>
+          }
+          <section className="score-assertion-section">
+            <div>
+              Score:
+              {' '}
+              <span data-testid="feedback-total-score">{ score }</span>
+            </div>
 
-        {
-          assertions < minAssertions
-            ? <h2 data-testid="feedback-text">Could be better... </h2>
-            : <h2 data-testid="feedback-text">Well Done! </h2>
-        }
-        <div>
-          Score:
-          {' '}
-          <span data-testid="feedback-total-score">{ score }</span>
-        </div>
+            <div>
+              Assertions:
+              {' '}
+              <span data-testid="feedback-total-question">{ assertions }</span>
+            </div>
+          </section>
 
-        <div>
-          Assertions:
-          {' '}
-          <span data-testid="feedback-total-question">{ assertions }</span>
-        </div>
-
-        <div>
-          <button
-            data-testid="btn-play-again"
-            type="button"
-            onClick={ () => history.push('/') }
-          >
-            Play again
-          </button>
-          <button
-            data-testid="btn-ranking"
-            type="button"
-            onClick={ this.returnToRankingPage }
-          >
-            Ranking
-          </button>
-        </div>
+          <section className="feedback-buttons-section">
+            <button
+              data-testid="btn-play-again"
+              type="button"
+              onClick={ () => history.push('/') }
+            >
+              Play again
+            </button>
+            <button
+              data-testid="btn-ranking"
+              type="button"
+              onClick={ this.returnToRankingPage }
+            >
+              Ranking
+            </button>
+          </section>
+        </main>
       </div>
     );
   }
